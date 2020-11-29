12d60b8b5206b1381bcb63272221427b3b861fb4
aksh-51n9h <singh2104.akshay@gmail.com>
2020-11-29 11:54:58 +0530

    platform independent file_path

--- dataset_preprocessing.py
+++ dataset_preprocessing.py
@@ -10,7 +10,7 @@ def split_data_set(n_rows=1000, train_test_split=0.1):
     :return: [x_train, y_train, x_test, y_test]
     """
     current_dir = os.getcwd()
-    file_path = os.path.join(current_dir, r"data_set\train\advertising.csv")
+    file_path = os.path.join(current_dir, "data_set", "train", "advertising.csv")
 
     df = pd.read_csv(file_path, nrows=n_rows)
 
--- logistic_regression_model.py
+++ logistic_regression_model.py
@@ -6,13 +6,13 @@ from logistic_regression import LogisticRegression
 if __name__ == "__main__":
     x_train, y_train, x_test, y_test = split_data_set(n_rows=1000, train_test_split=0.3)
 
-    model = LogisticRegression(learning_rate=0.1, max_iter=100, optimizer='gd', fit_intercept=True)
+    model = LogisticRegression(learning_rate=0.1, max_iter=1000, optimizer='gd', fit_intercept=True)
 
     model.fit(x_train, y_train)
     predictions = model.predict(x_test)
 
     print("--" * 30, "Logistic Regression (with Gradient Descent) Accuracy score:",
-          "Training samples: {0}, AUC on testing set: {1:.3f}".format(700, roc_auc_score(y_test, predictions)),
+          "Training samples: {0}, AUC on testing set: {1:.3f}".format(len(x_train), roc_auc_score(y_test, predictions)),
           "--" * 30,
           sep="\n")
 
@@ -22,6 +22,6 @@ if __name__ == "__main__":
     predictions = model.predict(x_test)
 
     print("--" * 30, "Logistic Regression (with Stochastic Gradient Descent) Accuracy score:",
-          "Training samples: {0}, AUC on testing set: {1:.3f}".format(700, roc_auc_score(y_test, predictions)),
+          "Training samples: {0}, AUC on testing set: {1:.3f}".format(len(x_train), roc_auc_score(y_test, predictions)),
           "--" * 30,
           sep="\n")
